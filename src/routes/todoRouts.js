e00833f722125e12600c4b3fbc9771c44b853d28
FATMEMEJOJ AKA Happy Gamer <ahmadjlad@hotmail.com>
2021-12-16 17:37:34 +0200

    add some modefication

--- src/routes/todoRouts.js
+++ src/routes/todoRouts.js
@@ -2,26 +2,26 @@ const express = require("express");
 const router = express.Router();
 const { Todo } = require("../models/index");
 const barber = require("../auth/middleware/bearer");
-router.get("/", barber, async (req, res) => {
+router.get("/", barber, async (req, res, next) => {
   try {
     const todos = await Todo.findAll();
     res.status(200).json(todos);
   } catch (error) {
     console.log(error);
-    res.send(500).json(error);
+    next(new Error(JSON.stringify(error)))
   }
 });
-router.post("/", barber, async (req, res) => {
+router.post("/", barber, async (req, res, next) => {
   try {
     const newTodo = await Todo.create(req.body);
 
     res.status(201).json(newTodo);
   } catch (error) {
     console.log(error);
-    res.status(500).json(error);
+    next(new Error(JSON.stringify(error)))
   }
 });
-router.put("/:id", barber, async (req, res) => {
+router.put("/:id", barber, async (req, res, next) => {
   console.log(req.params.id);
   try {
     console.log(req.body);
@@ -31,15 +31,16 @@ router.put("/:id", barber, async (req, res) => {
     res.status(201).json(newTodo);
   } catch (error) {
     console.log(error);
-    res.send(500).json(error);
+    next(new Error(JSON.stringify(error)))
   }
 });
-router.delete("/:id", barber, async (req, res) => {
+router.delete("/:id", barber, async (req, res, next) => {
   try {
     const newTodo = await Todo.destroy({ where: { id: req.params.id } });
     res.status(201).json(newTodo);
   } catch (error) {
-    res.send(500).json(error);
+    console.log(error);
+    next(new Error(JSON.stringify(error)))
   }
 });
 module.exports = router;
--- src/routes/userRouts.js
+++ src/routes/userRouts.js
@@ -2,17 +2,21 @@ const express = require("express");
 const basics = require("../auth/middleware/basics");
 const userModule = require("../auth/models/user");
 const userRouts = express.Router();
-userRouts.post("/sign-in", basics, (req, res) => {
-  const { user } = req.body;
-  res.json({
-    username: user.username,
-    email: user.email,
-    token: user.token,
-    capabilities: user.capabilities,
-  });
+userRouts.post("/sign-in", basics, (req, res, next) => {
+  try {
+    const { user } = req.body;
+    res.json({
+      username: user.username,
+      email: user.email,
+      token: user.token,
+      capabilities: user.capabilities,
+    });
+  } catch (error) {
+    next(new Error(JSON.stringify(error)))
+  }
 });
 
-userRouts.post("/sign-up", async (req, res) => {
+userRouts.post("/sign-up", async (req, res, next) => {
   try {
     const user = await userModule.createUser(req.body);
     console.log(req.body);
@@ -20,7 +24,7 @@ userRouts.post("/sign-up", async (req, res) => {
     res.status(201).json(user);
   } catch (e) {
     console.log(e);
-    res.status(500).json({ massage: e.massage });
+    next(new Error(JSON.stringify(e)))
   }
 });
 module.exports = userRouts;
