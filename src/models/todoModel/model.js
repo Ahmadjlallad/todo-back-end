b83b29fb88b9fe27f166e169cf8c70811c763dee
FATMEMEJOJ AKA Happy Gamer <ahmadjlad@hotmail.com>
2021-12-16 17:09:41 +0200

    change item to text

--- src/models/todoModel/model.js
+++ src/models/todoModel/model.js
@@ -1,7 +1,7 @@
 const todoModel = (seq, dataType) => {
   return seq.define("todo", {
     id: { type: dataType.STRING, primaryKey: true },
-    item: { type: dataType.STRING, allowNull: false },
+    text: { type: dataType.STRING, allowNull: false },
     assignedTo: { type: dataType.STRING, allowNull: false },
     difficulty: { type: dataType.STRING, allowNull: false },
     complete: { type: dataType.BOOLEAN, allowNull: false, defaultValue: false },
