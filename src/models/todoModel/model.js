11662df2e90804ac33545ab8c3d8b828b6240269
FATMEMEJOJ AKA Happy Gamer <ahmadjlad@hotmail.com>
2021-12-16 17:15:44 +0200

    assignedTo to assigned

--- src/models/todoModel/model.js
+++ src/models/todoModel/model.js
@@ -2,7 +2,7 @@ const todoModel = (seq, dataType) => {
   return seq.define("todo", {
     id: { type: dataType.STRING, primaryKey: true },
     text: { type: dataType.STRING, allowNull: false },
-    assignedTo: { type: dataType.STRING, allowNull: false },
+    assigned: { type: dataType.STRING, allowNull: false },
     difficulty: { type: dataType.STRING, allowNull: false },
     complete: { type: dataType.BOOLEAN, allowNull: false, defaultValue: false },
   });
